db087f746416872ce89555752e47a39a1bdcde4a
Matthew Dahl <mdahl@ciena.com>
2016-12-27 14:16:29 -0800

    Attempt to add config

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = {
-  rulesConfig: {
-    'ember-standard/destructure': [2, "always"]
+  configs: {
+    'ember-standard': {
+      rules: {
+        'ember-standard/destructure': [2, "always"],
+        'ember-standard/import': [2, "always"],
+        'ember-standard/logger': [2, "always"],
+        'ember-standard/no-set-in-computed-property': 2
+      }
+    }
   },
   rules: {
     'destructure': require('./rules/destructure')
